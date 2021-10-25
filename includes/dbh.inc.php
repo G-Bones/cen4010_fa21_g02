32808262fa38a4384ea392f6f03d62c185e06795
Hayden Stone <haydendavisstone@gmail.com>
2021-10-25 18:21:45 -0400

    removed changes for testing

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "localhost";
-$dBUsername = "root";
-$dBPassword = "";
-$dBName = "test";
+$servername = "lamp.cse.fau.edu";
+$dBUsername = "cen4010_fa21_g02@localhost";
+$dBPassword = "O7y4Zt8R5A";
+$dBName = "cen4010_fa21_g02";
 
 $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
 
